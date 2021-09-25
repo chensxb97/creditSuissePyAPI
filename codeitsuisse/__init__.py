ffcdbf8155db6407f033f083cab7e59dd371df54
chensxb97 <chensxb97@gmail.com>
2021-09-25 17:57:28 +0800

    submit question parasite

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 app = Flask(__name__)
 import codeitsuisse.routes.square
 import codeitsuisse.routes.parasite
-import codeitsuisse.routes.stonks
+# import codeitsuisse.routes.stonks
--- codeitsuisse/routes/stonks.py
+++ codeitsuisse/routes/stonks.py
@@ -33,11 +33,13 @@ logger = logging.getLogger(__name__)
 @app.route('/stonks', methods=['POST'])
 def timeTravel():
     data = request.get_json()
-    how is that mail.google.com
     logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input")
-    result = inputValue * inputValue
+    result = []
+    for case in data:
+        result.append(genOutput(case))
     logging.info("My result :{}".format(result))
     return json.dumps(result)
 
-    
+
+def genOutput(case):
+    return 1
